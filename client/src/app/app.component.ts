48700c901fc1870c73ad055463d4414c6003695f
krish <krishnaasinghbisht@gmail.com>
2024-09-04 00:52:52 +0530

    commit-4

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { NgFor } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,NgFor],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit{
+
+  title = 'Hostel-Outing-Application';
+  users: any;
+  http = inject(HttpClient);
+  
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get("http://localhost:5000/api/profile/students").subscribe({
+      next: (response) => {
+        this.users = response;
+        console.log(this.users)
+      },
+      error: err => console.log(err),
+      complete: () => console.log("request has completed!")
+    });
+  }
+}
---
+++ client/src/users/users.ts
@@ -0,0 +1,9 @@
+export interface Users {
+    id: string;
+    name: string;
+    roomNumber: string;
+    phoneNumber: string;
+    parentPhoneNumber: string;
+    address: string;
+    departmentAndCourse: string;
+}
